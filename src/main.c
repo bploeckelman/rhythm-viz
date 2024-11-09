45d7a0797d463780d4341ad5d99a4d1f9c7d68a8
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2024-11-09 17:29:10 -0600

    Add rlImGui and get wired into visual studio and building

--- src/main.c
+++ src/main.c
@@ -25,47 +25,46 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 */
 
 #include "raylib.h"
+#include "raymath.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "imgui.h"
+#include "rlImGui.h"
+
+#include "resource_dir.h"
 
 int main ()
 {
-	// Tell the window to use vysnc and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI);
 
-	// Create the window and OpenGL context
 	InitWindow(1280, 800, "Hello Raylib");
+	SetTargetFPS(144);
+
+	rlImGuiSetup(true);
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	while (!WindowShouldClose())
 	{
-		// drawing
 		BeginDrawing();
-
-		// Setup the backbuffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		// draw some text using the default font
 		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
 		DrawTexture(wabbit, 400, 200, WHITE);
+
+		rlImGuiBegin();
+		bool open = true;
+		ImGui::ShowDemoWindow(&open);
+		rlImGuiEnd();
 		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
 	UnloadTexture(wabbit);
 
-	// destory the window and cleanup the OpenGL context
+	rlImGuiShutdown();
 	CloseWindow();
 	return 0;
 }
